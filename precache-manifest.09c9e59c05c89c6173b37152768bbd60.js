bd8dcb970d1d61fad7b724b4c46ac38363895194
nurita18 <nurita.bc18@gmail.com>
2020-03-06 16:14:58 +0700

    deploy

---
+++ precache-manifest.09c9e59c05c89c6173b37152768bbd60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a0d8fe5a4c8c52137b56c0030f95d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f7eb64ffe40c07a0d89",
+    "url": "/static/css/main.63f295f2.chunk.css"
+  },
+  {
+    "revision": "8c23ef0be31bb9c62d07",
+    "url": "/static/js/2.94b7a682.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.94b7a682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f7eb64ffe40c07a0d89",
+    "url": "/static/js/main.2afac06d.chunk.js"
+  },
+  {
+    "revision": "2fc8e7369f6406e5984d",
+    "url": "/static/js/runtime-main.447fd809.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
